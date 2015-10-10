a9284e81d49b4af900017a490fc33250b4966800
Greta Huang <gretahuang@berkeley.edu>
2015-10-10 14:26:37 -0700

    Added usermixin functionality

--- app/models.py
+++ app/models.py
@@ -1,35 +1,21 @@
 from app import db
+from flask.ext.login import UserMixin
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
 
-class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    reviews = db.relationship('Post', backref='author', lazy='dynamic')
-
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
+    reviews = db.relationship('Review', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<Student %r>' % (self.nickname)
 
 
 class Review(db.Model):
+    __tablename__ = 'review'
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
--- config.py
+++ config.py
@@ -11,5 +11,5 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
-SQLALCHEMY_DATABASE_URI = "postgresql://gretahuang@localhost/gretahuang"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
