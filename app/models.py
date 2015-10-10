c442434f43573154652127dfb2728772bc006bbd
Tatevik Stepanyan <tatevikstepanyan@airbears2-10-142-67-252.AirBears2.1918.Berkeley.EDU>
2015-10-10 16:33:08 -0700

    models.py

--- app/models.py
+++ app/models.py
@@ -1,39 +1,36 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from app import db
+from datetime import datetime
 
+class User(db.Model):
+    __tablename__ = "students"
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    reviews = db.relationship('Post', backref='author', lazy='dynamic')
-
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
-
-    def __repr__(self):
-        return '<Student %r>' % (self.nickname)
-
+    ID = db.Column(db.Integer, primary_key=True, unique=True)
+    user = db.Column(db.String, unique=True)
+    fullname = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    reviews = db.relationship('Review', backref='students', lazy='dynamic', foreign_keys=[user_id])
 
 class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    __tablename__ = "reviews"
+
+    ID = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    propert = db.Column(db.String)
+    landlord = db.Column(Landlord, db.ForeignKey('landlord'))
 
-    def __repr__(self):
-        return '<Review %r>' % (self.body)
+class Landlord(Base):
+    __tablename__ = "landlords"
+
+    fullname = db.Column(db.String)
+    is_female = db.Column(db.Boolean, default=True)
+    race = db.Column(db.Integer)
+    hair_color = db.Column(db.Integer)
+    user = db.Column(db.String, primary_key=True)
+    total_rate = db.Column(db.Integer, default=0)
+    num_rates = db.Column(db.Integer, default=0)
+    avg_rating = db.Column(db.Float, default=0.0)
+    properties = db.relationship('Review.propert', backref='landlords', lazy='dynamic', primary_key=False, foreign_keys=[landlord])
