e67afbfda5db285bb51daf6d1ac8d9753348798f
Tatevik Stepanyan <tatevikstepanyan@airbears2-10-142-67-252.AirBears2.1918.Berkeley.EDU>
2015-10-10 17:49:29 -0700

    fixed foreign keys

--- app/models.py
+++ app/models.py
@@ -8,30 +8,33 @@ class User(UserMixin, db.Model):
     __tablename__ = "students"
 
     ID = db.Column(db.Integer, primary_key=True, unique=True)
-    user = db.Column(db.String, unique=True)
+    username = db.Column(db.String, unique=True)
     fullname = db.Column(db.String)
     email = db.Column(db.String, unique=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
     reviews = db.relationship('Review', backref='students', lazy='dynamic', foreign_keys=[user_id])
 
-class Review(db.Model):
-    __tablename__ = 'review'
-
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    propert = db.Column(db.String)
-    landlord = db.Column(Landlord, db.ForeignKey('landlord'))
-
-class Landlord(Base):
+class Landlord(db.Model):
     __tablename__ = "landlords"
 
+    landlord_id = db.Column(db.Integer, primary_key=True, unique=True)
     fullname = db.Column(db.String)
+    username = db.Column(db.String)
     is_female = db.Column(db.Boolean, default=True)
     race = db.Column(db.Integer)
     hair_color = db.Column(db.Integer)
-    user = db.Column(db.String, primary_key=True)
     total_rate = db.Column(db.Integer, default=0)
     num_rates = db.Column(db.Integer, default=0)
     avg_rating = db.Column(db.Float, default=0.0)
-    properties = db.relationship('Review.propert', backref='landlords', lazy='dynamic', primary_key=False, foreign_keys=[landlord])
+    landlord = db.Column(db.String, ForeignKey('landlord'))
+    properties = db.relationship('Review.propert', backref='landlords', lazy='dynamic', foreign_keys=[landlord]) #Review.propert? how to get attr not object
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    review_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    body = db.Column(db.String(140))
+    propert = db.Column(db.String)
+    landlord = db.Column('Landlord') #quotes?
